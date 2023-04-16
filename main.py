8339040e2fe6c20a2b4e602cc88963e27a1faf86
Victor Arruda <victorf.arruda@outlook.com>
2023-04-16 10:23:06 -0300

    melhoramento em main.py e no README.MD

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import logging
+import sys
 
 from decouple import config
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
+from src.choices import XPATH_REGION
 from src.crawler import CrawlerFinanceYahoo
 
-if __name__ == '__main__':
-    REGION = config('REGION', 'Brazil')
-    logging.info('INICIANDO...')
-    crawler = CrawlerFinanceYahoo(region=REGION)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+def main(region):
+    logging.info(f'INICIANDO... NA REGIÃO "{region}"')
+    crawler = CrawlerFinanceYahoo(region=region)
     logging.info('CRAWLER CRIADO')
     crawler.run()
+
+
+if __name__ == '__main__':
+    try:
+        REGION = " ".join(sys.argv[1:]) if not sys.argv[1:] == [] else config('REGION', 'Brazil')
+        region_xpath = XPATH_REGION.get(REGION)
+        if region_xpath is None:
+            raise Exception("REGIÃO NÃO PODE SER LOCALIZADA, VERIFIQUE SE DIGITOU CORRETAMENTE!!!")
+        else:
+            main(region=REGION)
+    except Exception as e:
+        logging.error(e)
